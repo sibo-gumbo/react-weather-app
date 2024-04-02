1e5d2147adc263d0b781240229cfde3d03f1b2c7
sibo gumbo <precygumbo@gmail.com>
2024-04-03 01:54:30 +0200

    Added Github repository link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Sibo Gumbo and is
+        <a
+          href="https://github.com/sibo-gumbo/react-weather-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
