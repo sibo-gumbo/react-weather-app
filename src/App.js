1dbc785c9a8be5954afcc327813a1f04b067409b
sibo gumbo <precygumbo@gmail.com>
2024-04-03 02:36:49 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Sibo Gumbo and is
-        <a
-          href="https://github.com/sibo-gumbo/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
           {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+          This project was coded by Sibo Gumbo and is
+          <a
+            href="https://github.com/sibo-gumbo/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
