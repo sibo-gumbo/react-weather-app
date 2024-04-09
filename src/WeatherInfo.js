588fa7bd78e4da3af1a0e688a2882777f3b9e864
sibo gumbo <precygumbo@gmail.com>
2024-04-09 04:36:29 +0200

    Created new components to display the icon, and weather unit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -15,16 +17,12 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="d-flex">
-              <img
-                src={props.data.iconUrl}
-                alt={props.data.description}
-                className="float-left"
-              />
               <div className="float-left">
-                <span className="temperature">
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span className="unit">°C</span>
+                <WeatherIcon code={props.data.icon} />
+              </div>
+
+              <div className="float-left">
+                <WeatherTemperature celsius={props.data.temperature} />
               </div>
             </div>
           </div>
