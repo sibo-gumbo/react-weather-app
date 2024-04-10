15590aed6e6dfffc6ffabd9690fa332a0040b479
sibo gumbo <precygumbo@gmail.com>
2024-04-11 00:57:48 +0200

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 import WeatherTemperature from "./WeatherTemperature";
 
@@ -17,11 +18,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="d-flex">
-              <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-night.png"
-                alt="rain-night"
-                className="float-left"
-              />
+              <WeatherIcon code={props.data.icon} />
               <div className="float-left">
                 <WeatherTemperature celsius={props.data.temperature} />
               </div>
